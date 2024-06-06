61e53ae4ff0b0850233fc1e39151dc0cfebc3a90
Shubham Londhe <shubhamlondhe63@gmail.com>
2024-06-06 17:16:26 +0530

    first reactive form creating started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './pages/student-form/student-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent
   ],
   imports: [
     BrowserModule,
